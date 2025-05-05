7fb7fcb7c04a57a2b0cb5f395c52dbe16bdbcfa2
Darcy <57480964+workin-at-the-car-walsh@users.noreply.github.com>
2025-05-05 12:24:33 +1000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -52,7 +52,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Lorem Ipsum",
 	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	links: [
